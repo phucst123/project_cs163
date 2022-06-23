2a454de8eae696ee8abba5ff93a057c2338c7f10
phucst123 <huuphucst123@gmail.com>
2022-06-23 16:12:03 +0700

    global variable declaration

---
+++ Global.h
@@ -0,0 +1,18 @@
+#ifndef GLOBAL
+#define GLOBAL
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+
+
+// #include "IOFile.h"
+#include "Trie.h"
+// #include "Menu.h"
+// #include "System.h"
+
+
+#endif
+
