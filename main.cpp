17ccb2e2cd4323c011beb473ddbb5994b1bcb32d
phucst123 <huuphucst123@gmail.com>
2022-06-24 23:37:42 +0700

    demo

--- main.cpp
+++ main.cpp
@@ -3,23 +3,45 @@
 using namespace std;
 
 int main() {
-	Trie* myTrie = new Trie;
-	
-	myTrie->insert("hi", "xin chao");
-	myTrie->insert("bye", "tam biet");
-	/*string c = myTrie->getMeaning("hi");
-	cout << c << endl;
+	//Trie* myTrie = new Trie;
+	//
+	//myTrie->insert("hi", "xin chao");
+	//myTrie->insert("bye", "tam biet");
+	///*string c = myTrie->getMeaning("hi");
+	//cout << c << endl;
 
-	myTrie->remove("hi");
-	string t = myTrie->getMeaning("hi");
-	cout << t << endl;
-	cout << "hi" << endl;*/
+	//myTrie->remove("hi");
+	//string t = myTrie->getMeaning("hi");
+	//cout << t << endl;
+	//cout << "hi" << endl;*/
 
-	if (!myTrie->root)
-		cout << "nullptr" << endl;
-	else
-		cout << "not null" << endl;
+	//myTrie->remove("go");
 
-	string tmp = "";
-	myTrie->display(cout);
+	//if (!myTrie->root)
+	//	cout << "nullptr" << endl;
+	//else
+	//	cout << "not null" << endl;
+
+	//string tmp = "";
+	//myTrie->display(cout);
+
+
+	loadData();
+	while (true) {
+		mainMenu();
+
+		saveData();
+
+		clearScreen();
+
+
+		cout << "Do you want to quit? (Y/N)" << endl;
+		string ch = "";
+		while (ch != "y" && ch != "Y" && ch != "n" && ch != "N") {
+			getline(cin, ch);
+		}
+		if (ch == "y" || ch == "Y") {
+			break;
+		}
+	}
 }
