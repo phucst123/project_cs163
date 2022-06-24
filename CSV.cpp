a547b1b5f50a1316b467256e5b6891c8111090e7
phucst123 <huuphucst123@gmail.com>
2022-06-24 23:37:15 +0700

    implement function read and save data to csv file

---
+++ CSV.cpp
@@ -0,0 +1,36 @@
+#include "Global.h"
+
+using namespace std;
+
+void loadData() {
+	readFromCSV("data\\engtoeng\\", ENGTOENG);
+}
+
+void saveData() {
+	saveToCSV("data\\engtoeng\\", ENGTOENG);
+}
+
+void readFromCSV(const std::string& filepath, Trie*& myTrie) {
+	string absoluteDir = filepath + "currentdata.csv";
+	ifstream fin(absoluteDir);
+
+	string line = "";
+	while (getline(fin, line)) {
+		string word, def;
+		stringstream ss(line);
+		getline(ss, word, ',');
+		getline(ss, def, ',');
+		//cout << word << ": " << def << endl;
+		line = "";
+		myTrie->insert(word, def);
+	}
+
+	//waitForEnter();
+}
+
+void saveToCSV(const std::string& filepath, Trie*& myTrie) {
+	string absoluteDir = filepath + "currentdata.csv";
+	ofstream fout(absoluteDir);
+	myTrie->display(fout, true);
+	fout.close();
+}
---
+++ CSV.h
@@ -0,0 +1,20 @@
+#ifndef CSV
+#define CSV
+
+#include "Global.h"
+#include "Trie.h"
+
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+void loadData();
+void saveData();
+
+void readFromCSV(const std::string& filepath, Trie*& myTrie);
+void saveToCSV(const std::string& filepath, Trie*& myTrie);
+
+
+
+#endif
