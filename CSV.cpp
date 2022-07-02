c906dd5f6b3b0af258aac375a1b9dfc2eef79323
phucst123 <huuphucst123@gmail.com>
2022-07-02 13:41:39 +0700

    add read and write favorite word list to file

--- CSV.cpp
+++ CSV.cpp
@@ -9,7 +9,12 @@ void loadData() {
 	//readFromCSV("data\\slang\\", SLANG);
 	//readFromCSV("data\\emoji\\", EMOJI);
 
-	//readList
+	readList("data\\engtoeng\\", FavorListEngToEng);
+	readList("data\\engtovie\\", FavorListEngToVie);
+	readList("data\\vietoeng\\", FavorListVieToEng);
+	readList("data\\slang\\", FavorListSlang);
+	readList("data\\emoji\\", FavorListEmoji);
+
 }
 
 void saveData() {
@@ -18,24 +23,31 @@ void saveData() {
 	//saveToCSV("data\\vietoeng\\", VIETOENG);
 	//saveToCSV("data\\slang\\", SLANG);
 	//saveToCSV("data\\emoji\\", EMOJI);
+
+	saveList("data\\engtoeng\\", FavorListEngToEng);
+	saveList("data\\engtovie\\", FavorListEngToVie);
+	saveList("data\\vietoeng\\", FavorListVieToEng);
+	saveList("data\\slang\\", FavorListSlang);
+	saveList("data\\emoji\\", FavorListEmoji);
 }
 
 void readFromCSV(const std::string& filepath, Trie*& myTrie) {
 	string absoluteDir = filepath + "currentdata.txt";
 	ifstream fin(absoluteDir);
-
-	string line = "";
-	while (getline(fin, line)) {
-		string word, def;
-		stringstream ss(line);
-		getline(ss, word, ':');
-		getline(ss, def, '\n');
-		//cout << word << ": " << def << endl;
-		line = "";
-		myTrie->insert(word, def);
+	if (!fin.is_open())
+		cout << "CANNOT OPEN FILE " << absoluteDir << endl;
+	else {
+		string line = "";
+		while (getline(fin, line)) {
+			string word, def;
+			stringstream ss(line);
+			getline(ss, word, ':');
+			getline(ss, def, '\n');
+			//cout << word << ": " << def << endl;
+			line = "";
+			myTrie->insert(word, def);
+		}
 	}
-
-	//waitForEnter();
 }
 
 //
@@ -64,25 +76,29 @@ void saveToCSV(const std::string& filepath, Trie*& myTrie) {
 
 
 void readList(const std::string& filepath, vector<pair<string, string>>& myList) {
-	ifstream fin(filepath);
-
-	string line = "";
-	while (getline(fin, line)) {
-		string word, def;
-		stringstream ss(line);
-		getline(ss, word, ':');
-		getline(ss, def, '\n');
-		//cout << word << ": " << def << endl;
-		line = "";
-		myList.push_back(make_pair(word, def));
+	string absoluteDir = filepath + "favor.txt";
+	ifstream fin(absoluteDir);
+	if (!fin.is_open())
+		cout << "CANNOT OPEN FILE " << absoluteDir << endl;
+	else {
+		string line = "";
+		while (getline(fin, line)) {
+			string word, def;
+			stringstream ss(line);
+			getline(ss, word, ':');
+			getline(ss, def, '\n');
+			//cout << word << ": " << def << endl;
+			line = "";
+			myList.push_back(make_pair(word, def));
+		}
+		fin.close();
 	}
-
-	fin.close();
 }
 
 void saveList(const std::string& filepath, vector<pair<string, string>>& myList) {
-	ofstream fout(filepath);
+	string absoluteDir = filepath + "favor.txt";
+	ofstream fout(absoluteDir);
 	for (auto x : myList)
-		fout << x.first << ": " << x.second << endl;
+		fout << x.first << ":" << x.second << endl;
 	fout.close();
 }
