a6e670029a329ac8c96debb215db3ccd5bf56c2a
duckhiem2003 <100983194+duckhiem2003@users.noreply.github.com>
2022-08-07 12:01:29 +0700

    Modify State.h

--- State.h
+++ State.h
@@ -18,8 +18,13 @@ private:
 	sf::Vector2f mousePosView;
 public:
 	State(sf::RenderWindow* window, std::stack<State*>* states);
+	virtual ~State();
+	Event ev;
 	const bool& getQuit() const;
 	void endState();
 	virtual void updateMousePos();
 	virtual void updateInput(const float& dt) = 0;
+	virtual void updateEvents() = 0;
+	virtual void update(const float& dt) = 0;
+	virtual void render(RenderTarget* target = nullptr) = 0;
 };
